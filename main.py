ff501febf83a560f684105e6429d3b1d98e8f2f0
thunderlol4444 <thunderjason4444@gmmail.com>
2024-09-25 17:33:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,5 +51,5 @@ app.add_middleware(
 )
 
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get('PORT', 8080)), log_level="info")
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get('PORT', 8080)), log_level="info")
