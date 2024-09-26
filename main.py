15bc9e826f8c3e02ed8565f3ca317268d77cfbd4
thunderlol4444 <thunderjason4444@gmmail.com>
2024-09-26 11:33:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ firebaseConfig = {
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    create_table()
+    # create_table()
     print("Connection established")
     yield
     print("Connection closed")
